c4eed89bf29950b8770756844fe8b38d622a1307
praju-1 <prajaktatayde0@gmail.com>
2021-05-03 15:38:11 +0530

    C progam for reversing sentence using Recurssion

---
+++ recurrsion/sentence_reverse/sentence_reverse.c
@@ -0,0 +1,22 @@
+//C program to Reverse a Sentence Using Recursion
+#include<stdio.h>
+
+void reverseSentence();
+int main()
+{
+    printf("Enter a sentence: ");
+    reverseSentence();
+    return 0;
+}
+
+// Recurssive function for reverse sentence
+void reverseSentence() 
+{
+    char c;
+    scanf("%c", &c);
+    if (c != '\n') 
+    {
+        reverseSentence();
+        printf("%c", c);
+    }
+}
