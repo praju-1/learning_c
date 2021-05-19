3d08225799ee390faadd2b8928e0c96012acdc9a
praju-1 <prajaktatayde0@gmail.com>
2021-05-19 17:09:09 +0530

    Program for swap number in cyclic order using call by reference.

---
+++ swap_cyclic/swap_cyclic.c
@@ -0,0 +1,33 @@
+// C Program Swap Numbers in Cyclic Order Using Call by Reference
+
+#include <stdio.h>
+void cyclicSwap(int *a, int *b, int *c);
+int main() 
+{
+    int a, b, c;
+
+    printf("Enter a, b and c respectively: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("Value before swapping:\n");
+    printf("a = %d \nb = %d \nc = %d\n", a, b, c);
+
+    cyclicSwap(&a, &b, &c);
+
+    printf("Value after swapping:\n");
+    printf("a = %d \nb = %d \nc = %d", a, b, c);
+
+    printf("\n");
+
+    return 0;
+}
+
+void cyclicSwap(int *n1, int *n2, int *n3) 
+{
+    int temp;
+    // swapping in cyclic order
+    temp = *n2;
+    *n2 = *n1;
+    *n1 = *n3;
+    *n3 = temp;
+}
